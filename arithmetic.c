0497ea881f9ab9edee09a07e244ade7199fc6b2d
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-09-09 18:21:37 +0600

    codeforce max-min problem solved

---
+++ arithmetic.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+// problem - 2
+
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     float division = a * 1.0 / b;
+//     printf("%d / %d = %.2f", a, b, division);
+//     return 0;
+// }
+
+// problem - 3
+
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if (a % 2 == 0)
+//     {
+//         printf("even");
+//     }
+//     else
+//     {
+//         printf("odd");
+//     }
+//     return 0;
+// }
+
+// problem - 4
+
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if (a > 0)
+//     {
+//         printf("positive");
+//     }
+//     else if (a < 0)
+//     {
+//         printf("negative");
+//     }
+//     else
+//     {
+//         printf("zero");
+//     }
+//     return 0;
+// }
+
+// Given a letter X. If the letter is lowercase print the letter after converting it from lowercase letter to uppercase letter. Otherwise print the letter after converting it from uppercase letter to lowercase letter
+
+// Note : difference between 'a' and 'A' in ASCII is 32 .
+
+// int main()
+// {
+//     char a;
+//     scanf("%c", &a);
+//     if (a >= 'a' && a <= 'z')
+//     {
+//         int ans = a - 32;
+//         printf("%c", ans);
+//     }
+//     else
+//     {
+//         int ans = a + 32;
+//         printf("%c", ans);
+//     }
+//     return 0;
+// }
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    // minimum
+    if (a <= b && a <= c)
+    {
+        printf("%d ", a);
+    }
+    else if (b <= a && b <= c)
+    {
+        printf("%d ", b);
+    }
+    else
+    {
+        printf("%d ", c);
+    }
+
+    // maximum
+    if (a >= b && a >= c)
+    {
+        printf("%d\n", a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("%d\n", b);
+    }
+    else
+    {
+        printf("%d\n", c);
+    }
+
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+// int main() {
+//    printf("Return something!\n"); // \n is an escape character for a new line
+
+//     // Printing another line of text
+//     printf("Here is the second line!\n"); // \n moves the cursor to the next line
+
+//     // Printing special characters
+//     printf("//\n"); // Prints two forward slashes and then moves to a new line
+//     printf("%%");   // Prints a percent sign (%)
+
+//    return 0;
+// }
+
+int main()
+{
+    printf("Hello, world! I am learning C programming language. ^_^\n");
+    printf("Programming is fun and challenging. /\\/\\/\\\n");
+    printf("I want to give my 100%% dedication to learn!\t");
+    printf("I will succeed one day.");
+    return 0;
+}
+
+// int main() {
+//     // Declare integer variables
+//     int count = 10;
+//     int countTwo = 20;
+
+//     // Declare float variable
+//     float a = 2.345;
+
+//     // Declare char variables
+//     char letter1 = 'A';
+//     char letter2 = 'B';
+//     char letter3 = '@';
+
+//     // Print integer variable using format specifier %d
+//     printf("%d\n", count);
+
+//     // Print two integer variables
+//     printf("%d %d\n", count, countTwo);
+
+//     // Print float variable using format specifier %f
+//     printf("%f\n", a);
+
+//     // Print float variable with 2 decimal places
+//     printf("%0.2f\n", a);
+
+//     // Print char variables using multiple %c format specifiers
+//     printf("%c %c %c\n", letter1, letter2, letter3);
+
+//     return 0;
+// }
+
+// int main(){
+//     int num, numTwo;
+//     char a, b;
+//     float f;
+
+//     scanf("%d", &num);
+//     printf("%d", num);
+//     return 0;
+// }
