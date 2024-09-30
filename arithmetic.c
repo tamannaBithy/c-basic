29499ab7307b3149bf0be2ba24e0747bf98265b0
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-09-30 19:59:48 +0600

    different kind of array operations

--- arithmetic.c
+++ arithmetic.c
@@ -53,55 +53,172 @@
 
 // Note : difference between 'a' and 'A' in ASCII is 32 .
 
-int main()
-{
-    char a;
-    scanf("%c", &a);
-    if (a >= 'a' && a <= 'z')
-    {
-        int ans = a - 32;
-        printf("%c", ans);
-    }
-    else
-    {
-        int ans = a + 32;
-        printf("%c", ans);
-    }
-    return 0;
-}
+// int main()
+// {
+//     char a;
+//     scanf("%c", &a);
+//     if (a >= 'a' && a <= 'z')
+//     {
+//         int ans = a - 32;
+//         printf("%c", ans);
+//     }
+//     else
+//     {
+//         int ans = a + 32;
+//         printf("%c", ans);
+//     }
+//     return 0;
+// }
 
 // between 3 number which one is min & which one is max
+// int main()
+// {
+//     int a, b, c;
+//     scanf("%d %d %d", &a, &b, &c);
+
+//     // minimum
+//     if (a <= b && a <= c)
+//     {
+//         printf("%d ", a);
+//     }
+//     else if (b <= a && b <= c)
+//     {
+//         printf("%d ", b);
+//     }
+//     else
+//     {
+//         printf("%d ", c);
+//     }
+
+//     // maximum
+//     if (a >= b && a >= c)
+//     {
+//         printf("%d\n", a);
+//     }
+//     else if (b >= a && b >= c)
+//     {
+//         printf("%d\n", b);
+//     }
+//     else
+//     {
+//         printf("%d\n", c);
+//     }
+
+//     return 0;
+// }
+
+// insert an element in an array
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int ar[n + 1];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &ar[i]);
+//     }
+
+//     int pos, val;
+//     scanf("%d %d", &pos, &val);
+
+//     for (int i = n; i >= pos + 1; i--)
+//     {
+//         ar[i] = ar[i - 1];
+//     }
+//     ar[pos] = val;
+//     for (int i = 0; i <= n; i++)
+//     {
+//         printf("%d ", ar[i]);
+//     }
+// }
+
+// remove an element from an array
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int ar[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &ar[i]);
+//     }
+
+//     int pos;
+//     scanf("%d", &pos);
+//     for (int i = pos; i < n - 1; i++)
+//     {
+//         ar[i] = ar[i + 1];
+//     }
+
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         printf("%d ", ar[i]);
+//     }
+// }
+
+// reversing an array, two pointers technique
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int ar[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &ar[i]);
+//     }
+
+//     int i = 0, j = n - 1;
+//     while (i < j)
+//     {
+//         int temp = ar[i];
+//         ar[i] = ar[j];
+//         ar[j] = temp;
+//         i++;
+//         j--;
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", ar[i]);
+//     }
+// }
+
+// copy an array
 int main()
 {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-
-    // minimum
-    if (a <= b && a <= c)
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int a[n], b[m];
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ", a);
+        scanf("%d", &a[i]);
     }
-    else if (b <= a && b <= c)
+    // int m;
+    // scanf("%d", &m);
+    // int b[m];
+    for (int i = 0; i < m; i++)
     {
-        printf("%d ", b);
-    }
-    else
-    {
-        printf("%d ", c);
+        scanf("%d", &b[i]);
     }
 
-    // maximum
-    if (a >= b && a >= c)
+    int ans[n + m];
+
+    for (int i = 0; i < n; i++)
     {
-        printf("%d\n", a);
+        ans[i] = a[i];
     }
-    else if (b >= a && b >= c)
+
+    int i = n;
+
+    for (int j = 0; j < m; j++)
     {
-        printf("%d\n", b);
+        ans[i] = b[j];
+        i++;
     }
-    else
+
+    for (int i = 0; i < n + m; i++)
     {
-        printf("%d\n", c);
+        printf("%d ", ans[i]);
     }
 
     return 0;
