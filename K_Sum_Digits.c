2322f227b71fd02809f9af0ec0a37de30a063625
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-10-05 14:25:53 +0600

    K. Sum Digits

--- K_Sum_Digits.c
+++ K_Sum_Digits.c
@@ -2,6 +2,21 @@
 
 int main()
 {
+    int N;
+    scanf("%d", &N);
+
+    char digits[N + 1];
+    scanf("%s", digits);
+
+    long long sum = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+
+        sum += digits[i] - '0';
+    }
+
+    printf("%lld\n", sum);
 
     return 0;
 }
