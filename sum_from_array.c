a0e536f63767a7f95c35da9d8a9d0a52a522fbf0
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-10-12 00:09:51 +0600

    selection sorting

---
+++ sum_from_array.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+// int main()
+// {
+//     int n, x;
+//     scanf("%d %d", &n, &x);
+//     int a[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+
+//     int false = 0;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+
+//             if (a[i] + a[j] == x)
+//             {
+//                 false = 1;
+//             }
+//         }
+//     }
+
+//     if (false == 1)
+//     {
+//         printf("yes");
+//     }
+//     else
+//     {
+//         printf("no");
+//     }
+
+//     return 0;
+// }
+
+//////////// sorting ////////////////////////
+
+/////////// ascending order
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int a[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+
+//             if (a[i] > a[j])
+//             {
+//                 int temp = a[i];
+//                 a[i] = a[j];
+//                 a[j] = temp;
+//             }
+//         }
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", a[i]);
+//     }
+
+//     return 0;
+// }
+
+////////// ascending order
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+
+            if (a[i] < a[j])
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
