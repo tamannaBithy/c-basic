50b942114cc125ab1d725d6a5b9e28f12d020f26
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-10-11 21:14:29 +0600

    pattern_5

--- pattern.c
+++ pattern.c
@@ -85,37 +85,55 @@
 
 /////////////////// diamond pattern //////////////////
 
+int main()
+{
+    int n, s, k;
+    scanf("%d", &n);
+    s = n - 1;
+    k = 1;
+    for (int i = 1; i <= (2 * n) - 1; i++)
+    {
+        for (int j = 1; j <= s; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= k; j++)
+        {
+            printf("*");
+        }
+        if (i <= n - 1)
+        {
+            s--;
+            k = k + 2;
+        }
+        else
+        {
+            s++;
+            k = k - 2;
+        }
+
+        printf("\n");
+    }
+}
+
 // int main()
 // {
-//     int n, s, k;
+//     int n, s, k = 1;
 //     scanf("%d", &n);
 //     s = n - 1;
-//     k = 1;
-//     for (int i = 1; i <= (2 * n) - 1; i++)
+//     for (int i = 0; i < n; i++)
 //     {
 //         for (int j = 1; j <= s; j++)
 //         {
 //             printf(" ");
 //         }
+
 //         for (int j = 1; j <= k; j++)
 //         {
 //             printf("*");
 //         }
-//         if (i <= n - 1)
-//         {
-//             s--;
-//             k = k + 2;
-//         }
-//         else
-//         {
-//             s++;
-//             k = k - 2;
-//         }
-
+//         s--;
+//         k++;
 //         printf("\n");
 //     }
 // }
-
-int main()
-{
-}
