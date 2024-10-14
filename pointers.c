c3a5bc88c9ec1118603624a5cbe625315b53f2f1
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-10-14 21:27:30 +0600

    basic pointers

--- pointers.c
+++ pointers.c
@@ -21,10 +21,15 @@ int main()
     int x = 10;
     //  printf("%p\n", x);
     int *p = &x; // aikhane muloto x er memory addressta e int p te rakha hocche
-    printf("%p\n", p);
+    // printf("%p\n", p);
 
     // dereference
 
-    printf("%d\n", *p);
+    printf("%d\n", *p); // kono memory address theke oi integerer value ber kora jay
+
+    *p = 300; // *p = x cause *p mane dereference , se ultimately main valuek e change kortece
+
+    printf("%d\n", x);
+
     return 0;
 }
