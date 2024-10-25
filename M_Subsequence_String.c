f8c353ac5610dcd246dded5188f2531746dc0ba1
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-10-25 21:29:02 +0800

    M_Subsequence String

--- M_Subsequence_String.c
+++ M_Subsequence_String.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    char s[10001];
 
+    scanf("%s", s);
+
+    char target[] = "hello";
+    int j = 0;
+
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+
+        if (s[i] == target[j])
+        {
+            j++;
+        }
+
+        if (j == 5)
+        {
+            printf("YES\n");
+            return 0;
+        }
+    }
+
+    printf("NO\n");
     return 0;
 }
