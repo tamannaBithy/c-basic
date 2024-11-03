6cdf33495045c69c96fe7c8d32e32b99f6526d4d
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-11-03 22:55:50 +0800

    G_Max and MIN

--- G_Max_and_MIN.c
+++ G_Max_and_MIN.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <limits.h>
+
+void fun(int arr[], int n)
+{
+    int max = INT_MIN;
+    int min = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+
+    printf("%d %d", min, max);
+}
 
 int main()
 {
+
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    fun(arr, n);
 
     return 0;
 }
