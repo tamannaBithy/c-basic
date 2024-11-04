729bba55e13d6fc7584f63459c0a6bd682657866
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-11-04 19:09:59 +0600

    C_Print from N to 1

---
+++ C_Print_from_N_to_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void fun(int n)
+{
+
+    if (n == 0)
+    {
+        return;
+    }
+    printf("%d ", n);
+    fun(n - 1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    fun(n);
+    return 0;
+}
