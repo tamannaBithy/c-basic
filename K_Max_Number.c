6256add834cca10e79036a7d966744f6822741b9
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-11-17 00:07:24 +0600

    K_Max Number

--- K_Max_Number.c
+++ K_Max_Number.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <limits.h>
+
+int fun(int arr[], int n, int i)
+{
+
+    if (i == n)
+    {
+        return INT_MIN;
+    }
+
+    int max = fun(arr, n, i + 1);
+
+    if (arr[i] > max)
+    {
+        return arr[i];
+    }
+    else
+    {
+        return max;
+    }
+}
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int result = fun(arr, n, 0);
+    printf("%d", result);
 
     return 0;
 }
